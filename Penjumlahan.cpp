4201ab72ea343e6a601ebec26032468dcb9089c1
anstasyakhrin <144661194+anstasyakhrin@users.noreply.github.com>
2023-09-21 12:11:50 +0700

    Create Penjumlahan.cpp

---
+++ Penjumlahan.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int a,b,c;
+  cout<<"Masukan Angka Pertama:";
+  cin>>a;
+  cout<<"Masukan Angka Kedua:";
+  cin>>b;
+  c=a+b;
+  cout<<"Hasilnya Adalah:"<<C<<endl;
+  return 0;
+ {
